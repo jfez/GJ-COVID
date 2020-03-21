0849dba92e4bf4225d64587f08c89866f2f010ad
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2020-03-21 11:21:31 +0100

    Camera

---
+++ Assets/Scripts/Camera/CameraRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public Transform player;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 direction = player.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+
+        float clampX = Mathf.Clamp(rotation.eulerAngles.x, 0, 65);
+        //float clampY = Mathf.Clamp(rotation.eulerAngles.y, -10, 60);
+        
+        
+        rotation.eulerAngles = new Vector3(clampX, 45, rotation.eulerAngles.z);
+
+        transform.rotation = rotation;
+        
+
+        //transform.LookAt(player);
+    }
+}
---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+    private bool pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause = false;
+        pauseCanvas.SetActive (false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    void Pause ()
+    {
+        if (pause)
+        {
+            Time.timeScale = 1f;
+            pauseCanvas.SetActive (false);
+
+            pause = false;
+        }
+
+        else
+        {
+            Time.timeScale = 0f;
+            pauseCanvas.SetActive (true);
+
+            pause = true;
+        }
+    }    
+
+}
